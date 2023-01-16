d0463796eab9e78ad3d45ba8f2a2cfecc0598fcb
mbernaciak <m.bernaciak.90@gmail.com>
2023-01-16 17:40:01 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import QuotesDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuotesDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+const QuotesDetail = () => {
+  return <h1>Quotes Detail Page</h1>;
+};
+
+export default QuotesDetail;
