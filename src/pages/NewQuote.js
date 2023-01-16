a52349cba67480078408d1b8fbc4af084979aae7
mbernaciak <m.bernaciak.90@gmail.com>
2023-01-16 19:44:46 +0100

    form focus and prompt

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,10 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useHistory } from "react-router-dom";
 
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
+    history.push("./quotes");
     console.log(quoteData);
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
