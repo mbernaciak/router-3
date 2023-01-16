31e5544660e53c8b996717bbcaefdd40bd1560a8
mbernaciak <m.bernaciak.90@gmail.com>
2023-01-16 18:44:37 +0100

    after Navigation

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuotesList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuotesList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
